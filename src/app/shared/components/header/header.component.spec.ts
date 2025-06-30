7d49851b2d81785203f6f58fea93cefd10a7b269
Tales Eduardo <tales.silva@psvsistemas.com.br>
2025-06-30 15:01:59 -0300

    feat: cria header

---
+++ src/app/shared/components/header/header.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      imports: [HeaderComponent],
+    });
+
+    await TestBed.compileComponents();
+
+    fixture = TestBed.createComponent(HeaderComponent);
+    fixture.detectChanges();
+  });
+
+  it('deve renderizar o título corretamente', () => {
+    const h1DebugEl = fixture.debugElement.query(By.css('h1'));
+    expect(h1DebugEl.nativeElement.textContent).toBe('Gerenciador de Tarefas');
+  });
+});
