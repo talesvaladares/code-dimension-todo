c27f6d575b60fce0618519d4852c7c6fa71d2731
Tales Eduardo <tales.silva@psvsistemas.com.br>
2025-07-01 19:18:24 -0300

    feat(NoItemsComponent): cria componente

---
+++ src/app/pages/list/components/no-items/no-items.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-no-items',
+  imports: [CommonModule],
+  templateUrl: './no-items.component.html',
+  styleUrl: './no-items.component.css',
+})
+export class NoItemsComponent {}
