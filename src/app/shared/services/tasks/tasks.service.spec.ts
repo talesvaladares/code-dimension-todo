a0ee47388532f95783ad5c0b624b6728d03bed8c
Tales Eduardo <tales.silva@psvsistemas.com.br>
2025-07-01 10:59:11 -0300

    feat(TasksService): utiliza httpClient para fazer requisições

--- src/app/shared/services/tasks/tasks.service.spec.ts
+++ src/app/shared/services/tasks/tasks.service.spec.ts
@@ -2,13 +2,23 @@ import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { TasksService } from './tasks.service';
 import { Task } from '../../types/task';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
 
 describe('TasksService', () => {
   let service: TasksService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
+    
     service = TestBed.inject(TasksService);
+    httpTestingController = TestBed.inject(HttpTestingController)
   });
 
   it('getAll() deve retornar uma lista de tarefas', fakeAsync(() => {
@@ -17,15 +27,20 @@ describe('TasksService', () => {
       result = tasks
     });
 
-    tick();
-
-    expect(result).toEqual([
+    const request = httpTestingController.expectOne('/tasks');
+    const fakeTasks: Task[] = [
       {title: 'Item 1', completed: false},
       {title: 'Item 2', completed: false},
       {title: 'Item 3', completed: false},
       {title: 'Item 4', completed: true},
       {title: 'Item 5', completed: true},
       {title: 'Item 6', completed: true},
-    ]);
+    ];
+    
+    request.flush(fakeTasks);
+
+    tick();
+
+    expect(result).toEqual(fakeTasks);
   }));
 });
--- src/app/shared/services/tasks/tasks.service.ts
+++ src/app/shared/services/tasks/tasks.service.ts
@@ -1,21 +1,17 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Task } from '../../types/task';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
+  private httpCliente = inject(HttpClient);
+
   public getAll(): Observable<Task[]> {
-    return of([
-      {title: 'Item 1', completed: false},
-      {title: 'Item 2', completed: false},
-      {title: 'Item 3', completed: false},
-      {title: 'Item 4', completed: true},
-      {title: 'Item 5', completed: true},
-      {title: 'Item 6', completed: true},
-    ]);
+    return this.httpCliente.get<Task[]>('/tasks');
   }
 
 }
