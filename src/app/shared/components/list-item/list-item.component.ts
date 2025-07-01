db6df0b0d481fd1421b8537b28841b88c361efa9
Tales Eduardo <tales.silva@psvsistemas.com.br>
2025-07-01 19:35:21 -0300

    feat(ListItemComponent): cria componente

---
+++ src/app/shared/components/list-item/list-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-list-item',
+  imports: [CommonModule],
+  templateUrl: './list-item.component.html',
+  styleUrl: './list-item.component.css',
+})
+export class ListItemComponent {}
