622bb1c8548296ec5f4bcf7468009c384497f0e2
Tales Eduardo <tales.silva@psvsistemas.com.br>
2025-07-02 19:53:54 -0300

    feat: implementa tamplate do listItemComponent

--- src/app/shared/components/list-item/list-item.component.spec.ts
+++ src/app/shared/components/list-item/list-item.component.spec.ts
@@ -1,9 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListItemComponent } from './list-item.component';
+import { By } from '@angular/platform-browser';
+import { Task } from '../../types/task';
+import { TestHelper } from '@testing/helpers/test-helper'
 
 describe('ListItemComponent', () => {
-  let component: ListItemComponent;
   let fixture: ComponentFixture<ListItemComponent>;
+  let testHelper: TestHelper<ListItemComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -11,11 +14,20 @@ describe('ListItemComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(ListItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    testHelper = new TestHelper(fixture)
   });
+  
+  it('deve renderizar o título da tarefa', () => {
+    const fakeTask: Task = {
+      title: 'Nome da tarefa',
+      completed: false
+    }
+    
+    fixture.componentRef.setInput('task', fakeTask);
+    fixture.detectChanges();
+    
+    const text = testHelper.getTextContentByTestId('list-item-task-title');
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(text).toBe(fakeTask.title);
   });
 });
--- src/app/shared/components/list-item/list-item.component.ts
+++ src/app/shared/components/list-item/list-item.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Task } from '../../types/task';
 
 @Component({
   selector: 'app-list-item',
@@ -7,4 +8,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './list-item.component.html',
   styleUrl: './list-item.component.css',
 })
-export class ListItemComponent {}
+export class ListItemComponent {
+  task = input.required<Task>()
+}
